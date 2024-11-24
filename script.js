4f18ad70d623c95d246d9682a27a8dbc768b92c3
Josevan <134022658+rytheum@users.noreply.github.com>
2024-11-24 20:39:23 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-const hamburger = document.getElementById("hamburger");
-const navbar = document.getElementById("navbar");
+function toggleMenu() {
+  const navbarUl = document.querySelector('.navbar ul');
+  const menuIcon = document.querySelector('.menu-icon');
+  navbarUl.classList.toggle('show');
+  menuIcon.classList.toggle('open');
+}
 
-hamburger.addEventListener("click", () => {
-  navbar.classList.toggle("active");
-});
+function handleScroll() {
+  const boxes = document.querySelectorAll('.box');
+  const triggerBottom = window.innerHeight / 5 * 4;
+
+  boxes.forEach(box => {
+      const boxTop = box.getBoundingClientRect().top;
+
+      if (boxTop < triggerBottom) {
+          box.style.opacity = '1';
+          box.style.transform = 'translateY(0)';
+      } else {
+          box.style.opacity = '0';
+          box.style.transform = 'translateY(20px)';
+      }
+  });
+}
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('load', handleScroll);
